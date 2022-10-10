640ceb094ba8573b38242051dacb57c96179fd91
cipaolini <chiara.paolini4@gmail.com>
2022-10-10 18:05:46 +0200

    Dative changes

--- R/variables.R
+++ R/variables.R
@@ -1,14 +1,14 @@
 # For other datasets you may want to change these variables
 
 nephovis_link <- function(lemma, model) {
-  glue::glue("https://qlvl.github.io/NephoVis/level3.html?type={lemma}&model={model}")
+  "#"
 }
 
-hdbscan_dir <- here::here("cloudspotting-medoids", "hdbscan")
+hdbscan_dir <- here::here("DativeVis", "clustering")
 
-wwmx_dir <- here::here("cloudspotting-medoids", "wwmx")
+wwmx_dir <- here::here("DativeVis", "wwmx")
 lemmas <- dir(wwmx_dir)
-n_medoids <- 8
-starting_lemma <- "heffen"
+n_medoids <- 1
+starting_lemma <- "give"
 
-sense_column <- "collapsed_sense"
+sense_column <- "variant"
